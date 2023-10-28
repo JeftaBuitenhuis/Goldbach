2045aa03ec083133d779c032071646a116657134
Jefta <buitenhuis.jefta@gmail.com>
2023-10-28 17:50:38 +0200

    current cpp version

--- src/goldbach.cpp
+++ src/goldbach.cpp
@@ -1,5 +1,3 @@
-// CUDA runtime
-#include <cuda_runtime.h>
 //#include <immintrin.h> // SIMD AVX intel library
 //#include <ia32intrin.h> // SIMD SVML intel library
 #include <atomic> // atomic
@@ -9,29 +7,13 @@
 #include <math.h> // Mathematics functions
 #include <tuple> // tuple
 
-__global__ void atkin_conditions(unsigned long max, unsigned long x, bool *p){
-    unsigned long y = threadIdx.x + blockDim.x*blockIdx.x;
-    //printf("%d\n", y);
-
-    // Condition 1
-    unsigned long n = (4 * x * x) + (y * y);
-    if (n <= max && (n % 12 == 1 || n % 12 == 5)){
-        p[n] ^= true;
-    }
-
-    // Condition 2
-    n = (3 * x * x) + (y * y);
-    if (n <= max && n % 12 == 7){
-        p[n] ^= true;
-    }
-
-    // Condition 3
-    n = (3 * x * x) - (y * y);
-    if (x > y && n <= max && n % 12 == 11){
-        p[n] ^= true;
-    }
-}
-
+/**
+ * A class for calculating goldbach.
+ * 
+ * @author J.J.Buitenhuis
+ * @version 0.2
+ * @since 0.0
+*/
 class Goldbach{
     public:
         /**
@@ -40,13 +22,12 @@ class Goldbach{
          * @param max the maximum value calculated
          * @param threads the amount of CPU threads used for calculations
         */
-        Goldbach(unsigned long max, int threads, int g_threads){
+        Goldbach(int max, int threads){
             THREADS = threads;
             MAX = max;
-            CACHE_P = new bool[max + 1];
-            CACHE_G = new unsigned long[max];
+            CACHE_P = new bool[max];
+            CACHE_G = new int[max];
             CURRENT_NUM = MAX;
-            G_THREADS = g_threads;
         }
         // FUNCTIONS
         void gen_primes();
@@ -55,32 +36,33 @@ class Goldbach{
         void initialize_threads();
     private:
         // VARIABLES
-        unsigned long MAX;
+        int MAX;
         int THREADS = 1;
-        int G_THREADS = 1024;
+        std::atomic_int NUM = 7;
         //int SECTIONS;
-        std::atomic_long CURRENT_NUM = 2;
+        std::atomic_int CURRENT_NUM = 2;
         //int CURRENT_NUM = 2;
         bool *CACHE_P;
         
 
         // FUNCTIONS
-        void solve_goldbach(unsigned long);
+        void cache_primes();
+        bool is_prime(int);
+        void solve_goldbach(int);
         void balance_load();
 
         // OUTPUT
-        unsigned long *CACHE_G;
+        int *CACHE_G;
 };
 
 //----------------------------------------------------// Goldbach
 
-void Goldbach::solve_goldbach(unsigned long num){
-    unsigned long i = 2;
+void Goldbach::solve_goldbach(int num){
+    int i = 2;
     bool stop = (CACHE_P[i] && CACHE_P[num-i]);
     i--;
     while (!stop){
         i+=2;
-        //std::cout << i << "\n";
         stop = (CACHE_P[i] && CACHE_P[num-i]);
     }
     CACHE_G[num] = num-i;
@@ -101,7 +83,7 @@ void Goldbach::initialize_threads(){
 
 // void BalanceLoad()
 void Goldbach::balance_load(){
-    unsigned long n = (CURRENT_NUM -= 2);
+    int n = (CURRENT_NUM -= 2);
 
     while (n >= 4){
         solve_goldbach(n);
@@ -113,38 +95,44 @@ void Goldbach::balance_load(){
 //----------------------------------------------------// Primes
 
 void Goldbach::gen_primes(){
-    bool *d_x;//*h_x, *d_x;
-    int nblocks = 1, nthreads=G_THREADS;
-
-    //CACHE_P = (bool *) malloc(MAX*sizeof(bool) + sizeof(bool));
-    cudaMalloc((void **) &d_x, MAX*sizeof(bool) + sizeof(bool));
-
     CACHE_P[2] = true;
     CACHE_P[3] = true;
+    CACHE_P[5] = true;
+    CACHE_P[7] = true;
+    std::vector<std::thread> thread_vector;
 
-    for (unsigned long i = 0; i*i < MAX; i++){
-        atkin_conditions<<<nblocks,nthreads>>>(MAX, i, d_x);
+    for (int thread = 0; thread < THREADS; thread++){
+        thread_vector.emplace_back([&](){(cache_primes());});
     }
-
-    cudaMemcpy(CACHE_P,d_x,MAX, cudaMemcpyDeviceToHost);
-
-
-    // Mark all multiples
-    // of squares as non-prime
-    for (unsigned long r = 5; r * r <= MAX; r++) {
-        if (CACHE_P[r]) {
-            for (unsigned long i = r * r; i <= MAX; i += r * r)
-                CACHE_P[i] = false;
-        }
+    for(auto& t: thread_vector){
+        t.join();
     }
-    cudaFree(d_x);
+}
+
+void Goldbach::cache_primes(){
+    int n = (NUM += 2);
+    while (NUM < MAX){
+        CACHE_P[n] = is_prime(n);
+        n = NUM += 2;
+    }
+}
+
+bool Goldbach::is_prime(int n){
+    int sqrt_n = std::sqrt(n);
+    int i = 5;
+    bool mod_z = (n % 3);
+    while (mod_z && i <= sqrt_n){
+        mod_z = (n % i && n % (i + 2));
+        i += 6;
+    }
+    return mod_z;
 }
 
 //----------------------------------------------------------------// Debug
 
 void Goldbach::write_cache_p(){
-    unsigned long amount = 0;
-    for (unsigned long i = 0; i < MAX; i++){
+    int amount = 0;
+    for (int i = 0; i < MAX; i++){
         if (CACHE_P[i]) {
             amount++;
         }
@@ -154,10 +142,10 @@ void Goldbach::write_cache_p(){
 }
 
 void Goldbach::write_cache_g(){
-    unsigned long i_one_high = 0;
-    unsigned long i_two_high = 0;
-    for (unsigned long i = 4; i < MAX; i += 2){
-        //std::cout << i << " = " << CACHE_G[i] << " + " << CACHE_G[i+1]  << "\n";
+    int i_one_high = 0;
+    int i_two_high = 0;
+    for (int i = 4; i < MAX; i += 2){
+        std::cout << i << " = " << CACHE_G[i] << " + " << CACHE_G[i+1]  << "\n";
         if (i_one_high < CACHE_G[i]){
             i_one_high = CACHE_G[i];
         }
@@ -170,17 +158,12 @@ void Goldbach::write_cache_g(){
 
 //---------------------------------------------------------------// Main
 
-int main(int argc, char* argv[]){
-    unsigned long max = std::strtoul(argv[1], NULL, 0);
-    int threads = std::stoi(argv[2]);
-    int g_threads = std::stoi(argv[3]);
-    bool debug = std::stoi(argv[4]);
-    if (debug){
-        //get_device_data();
-    }
-    Goldbach goldbach(max, threads, g_threads);
+int main(){
+    int num = 10000000;
+    int amount_of_threads = 8;
+    Goldbach goldbach(num, amount_of_threads);
     goldbach.gen_primes();
     goldbach.initialize_threads();
-    //goldbach.write_cache_p();
     //goldbach.write_cache_g();
+    //goldbach.write_cache_p();
 }
